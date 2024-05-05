36d329bde02958ab5d1c4f8c22d6e44d0c895379
DESKTOP-867NNIC\Admin <ponlakritkm@gmail.com>
2024-05-05 21:52:02 +0700

    feat: 🎸 Addd navbar, sidebar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./layouts/sidebar/sidebar.component";
+import { NavbarComponent } from "./layouts/navbar/navbar.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, SidebarComponent, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   
